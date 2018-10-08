2ebca2c40c097dc175210237a6fe9828af0a116d
Michal Chmielarz <michal.chmielarz@gmail.com>
2018-10-08 23:19:48 +0200

    Application startup

--- src/main/java/pl/devthoughts/jsr380/Application.java
+++ src/main/java/pl/devthoughts/jsr380/Application.java
@@ -1,6 +1,13 @@
 package pl.devthoughts.jsr380;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-class Application {}
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
